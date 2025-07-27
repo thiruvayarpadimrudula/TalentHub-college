398106d084dc8801462b2553f0194bf0c446ba90
thiruvayarpadimrudula <145481921+thiruvayarpadimrudula@users.noreply.github.com>
2025-07-27 13:03:12 +0530

    Update UploadPage.jsx

--- talenthub-frontend/src/pages/UploadPage.jsx
+++ talenthub-frontend/src/pages/UploadPage.jsx
@@ -26,7 +26,7 @@ const UploadPage = () => {
     }
 
     try {
-      const res = await axios.get("https://talenthub-college-backend1.onrender.com/api/projects", formData, {
+      const res = await axios.post('http://talenthub-college-backend1.onrender.com/api/projects', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
