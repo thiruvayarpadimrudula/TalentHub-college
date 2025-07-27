0170089e4230464d47817352c0a45aea47936d5b
thiruvayarpadimrudula <145481921+thiruvayarpadimrudula@users.noreply.github.com>
2025-07-27 13:18:31 +0530

    Update server.js

--- talenthub-backend/server.js
+++ talenthub-backend/server.js
@@ -10,10 +10,15 @@ const app = express();
 dotenv.config();
 
 // Middlewares
+// app.use(cors({
+//   origin: ['http://localhost:3000', 'https://talenthub-college-frontend1.onrender.com'],
+//   credentials: true
+// }));app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://talenthub-college-frontend1.onrender.com'],
+  origin: ['https://talenthub-college-frontend1.onrender.com'],
+  methods: ['GET', 'POST'],
   credentials: true
-}));app.use(express.json());
+}));
 
 // Static files - VERY IMPORTANT
 const __dirname = path.resolve();
